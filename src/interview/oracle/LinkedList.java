beddefcbe0856bc9c8d1f5cb5fd30cc8d93bab7b
kakuma <kakuma@KAKUMA-IN.oradev.oraclecorp.com>
2017-04-28 16:47:56 +0530

    LinkedList and List reversal

--- src/interview/oracle/LinkedList.java
+++ src/interview/oracle/LinkedList.java
@@ -5,13 +5,34 @@ public class LinkedList {
 	public boolean add(int value){
 		Node node = new Node(value);
 		Node curr = head;
-		while(curr.getNext() != null);
-		curr.setNext(node);
+		if(curr == null) {
+			head = node;
+			return true;
+		}
+		try {
+			while(curr.getNext() != null){
+				curr = curr.getNext();
+			}
+			curr.setNext(node);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}		
 		return true;
 	}
-	public void toString(Node head){
-		
-		while()
+	public String toString(){
+		Node curr = head;
+		String retVal = "[";
+		while(curr !=null) {
+			retVal+=curr.getValue();
+			if(curr.getNext()!=null){
+				retVal+=", ";
+			}
+			curr = curr.getNext();
+		}
+		retVal+="]";
+		return retVal;
 	}
 	
 }
