0baa955d9117bbf95b7206302b7923a9636f75be
shailendra <shailendra.25.vits@gmail.com>
2017-05-04 22:28:48 +0530

    Reversing linked list

--- src/interview/oracle/ReverseList.java
+++ src/interview/oracle/ReverseList.java
@@ -10,22 +10,83 @@ public class ReverseList {
 		lst.add(5);
 		lst.add(6);
 		lst.add(7);
-		reverseListInPair(lst);
-		//reverseList(lst);
+		System.out.println(lst);
+		lst.head = reverseListRecur(lst.head);
 		System.out.println(lst);
 	}
-	private static void reverseList(LinkedList lst){
-		Node head = lst.head;
-		Node revNext = null;
-		while(head.getNext()!=null) {
-			Node temp = head.getNext();
-			head.setNext(revNext);
-			revNext = head;
-			head = temp;
+
+	private static Node reverseListInPairRecur(Node head) {
+		
+		
+		if(head==null || head.next==null){
+			return head;
+			
 		}
-		head.setNext(revNext);
-		lst.head = head ;
+		
+		
+		Node remaining= head.next.next;
+		
+		Node newHead=head.next;
+		
+		head.next.next=head;
+		
+		head.next=reverseListInPairItr(remaining);
+		
+		return newHead;
+		
 	}
+	
+	
+	private static Node reverseListInPairItr(Node head) {
+		
+		if(head ==null || head.next==null){
+			return head;
+		}
+		
+		Node p=head;
+		Node newHead=p.next;
+		
+		while(true){
+			Node q=p.next;
+			Node temp=q.next;
+			q.next=p;
+			if(temp==null || temp.next==null){
+				p.next=temp;
+				return newHead;
+			}
+			
+			p.next=temp.next;
+			p=temp;
+		}
+		
+	}
+	
+	private static Node reverseListItr(Node head){
+		Node curr=head;
+		Node prev=null;
+		while(curr!=null){
+			Node next = curr.next;
+			curr.next=prev;
+			prev=curr;
+			curr=next;
+		}
+		return prev;
+	}
+	
+	private static Node reverseListRecur(Node head){
+		
+		if(head==null || head.next==null){
+			return head;
+		}
+		
+		Node next=reverseListRecur(head.next);
+		
+		head.next.next=head;
+		head.next=null;
+		return next;
+	}
+	
+	
 	private static void reverseListInPair(LinkedList lst) {
 		Node head = lst.head;
 		Node revNext = null;
@@ -43,6 +104,8 @@ public class ReverseList {
 			head.getNext().setNext(revNext);
 		}
 		lst.head = head ;
-	}
+	} 
+	
+	
 
 }
